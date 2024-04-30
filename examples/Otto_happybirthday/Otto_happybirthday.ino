dce858090e8b6a7678e95d689011164abe6feb9d
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 01:07:52 +0600

    comment

--- examples/Otto_happybirthday/Otto_happybirthday.ino
+++ examples/Otto_happybirthday/Otto_happybirthday.ino
@@ -1,16 +1,10 @@
 //----------------------------------------------------------------
 //-- Otto Happy Birthday
-//-- With this code Otto will walk 5 steps and then sing the happy birthday
-//-- Otto DIY invests time and resources providing open source code and hardware, 
-//-- please support by purchasing kits from https://www.ottodiy.com/
-//-- Make sure to have installed all libraries: https://github.com/OttoDIY/OttoDIYLib
-//-----------------------------------------------------------------
-#include <Otto.h>
-Otto Otto;
-
-#define LeftLeg 2 
-#define RightLeg 3
-#define LeftFoot 4 
-#define RightFoot 5 
-#define Buzzer  13 
-#define LED     13  // Use the same pin for the LED 
+//-- This code makes Otto, a DIY robot, walk 5 steps and then sing the
+//-- happy birthday song. 
+//-- Otto DIY is an open source hardware and software project that
+//-- encourages community involvement and support. Please consider
+//-- purchasing kits from their website: https://www.ottodiy.com/
+//-- Make sure to install all necessary libraries before running
+//-- this code: https://github.com/OttoDIY/OttoDIYLib
+//--------------------------------

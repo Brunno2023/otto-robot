cf59700ce611064172dc470a0db41150645a14c2
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 01:08:09 +0600

    comment

--- src/Oscillator.cpp
+++ src/Oscillator.cpp
@@ -3,43 +3,90 @@
 
 #include "Arduino.h"
 
+/**
+ * @class Oscillator
+ * @brief This class implements an oscillator that generates a sine wave.
+ * The oscillator can be attached to a servo motor to physically observe
+ * the sine wave.
+ */
 class Oscillator {
 public:
-    Oscillator();
-    ~Oscillator();
+    /**
+     * @brief Constructs an Oscillator object with default values.
+     */
+    Oscillator() noexcept;
+
+    /**
+     * @brief Destroys the Oscillator object.
+     */
+    ~Oscillator() noexcept = default;
+
+    /**
+     * @brief Advances the oscillator to the next sample.
+     * @return True if a new sample is available, false otherwise.
+     */
     bool next_sample() noexcept;
+
+    /**
+     * @brief Attaches the oscillator to a servo motor.
+     * @param pin The pin number to attach the servo to.
+     * @param rev If true, the sine wave will be inverted.
+     */
     void attach(int pin, bool rev) noexcept;
+
+    /**
+     * @brief Detaches the oscillator from the servo motor.
+     */
     void detach() noexcept;
+
+    /**
+     * @brief Sets the period of the sine wave in microseconds.
+     * @param T The period of the sine wave.
+     */
     void SetT(unsigned int T) noexcept;
+
+    /**
+     * @brief Sets the position of the servo motor.
+     * @param position The position of the servo motor.
+     */
     void SetPosition(int position) noexcept;
+
+    /**
+     * @brief Updates the oscillator's internal state.
+     */
     void refresh() noexcept;
+
+    /**
+     * @brief Writes the position of the servo motor.
+     * @param position The position of the servo motor.
+     */
     void write(int position) noexcept;
 
 private:
-    Servo _servo;
-    unsigned long _currentMillis;
-    unsigned long _previousMillis;
-    unsigned long _previousServoCommandMillis;
-    unsigned int _samplingPeriod;
-    unsigned int _period;
-    unsigned int _numberSamples;
-    float _inc;
-    float _amplitude;
-    float _phase;
-    float _phase0;
-    float _offset;
-    bool _rev;
-    bool _stop;
-    int _pos;
-    int _trim;
-    int _diff_limit;
+    Servo _servo; // The servo motor.
+    unsigned long _currentMillis; // The current time in milliseconds.
+    unsigned long _previousMillis; // The previous time in milliseconds.
+    unsigned long _previousServoCommandMillis; // The previous time a servo command was sent.
+    unsigned int _samplingPeriod; // The time between samples in milliseconds.
+    unsigned int _period; // The period of the sine wave in microseconds.
+    unsigned int _numberSamples; // The number of samples in one period.
+    float _inc; // The increment in radians per sample.
+    float _amplitude; // The amplitude of the sine wave.
+    float _phase; // The phase of the sine wave.
+    float _phase0; // The initial phase of the sine wave.
+    float _offset; // The offset of the sine wave.
+    bool _rev; // If true, the sine wave will be inverted.
+    bool _stop; // If true, the oscillator will stop generating new samples.
+    int _pos; // The current position of the servo motor.
+    int _trim; // The trim value for the servo motor.
+    int _diff_limit; // The maximum difference in position between samples.
 };
 
 #endif /* OSCILLATOR_H_ */
 
-
-#include "Oscillator.h"
-
+/**
+ * @brief Constructs an Oscillator object with default values.
+ */
 Oscillator::Oscillator() noexcept
     : _servo(),
       _currentMillis(0),
@@ -61,8 +108,10 @@ Oscillator::Oscillator() noexcept
     // empty
 }
 
-Oscillator::~Oscillator() noexcept = default;
-
+/**
+ * @brief Advances the oscillator to the next sample.
+ * @return True if a new sample is available, false otherwise.
+ */
 bool Oscillator::next_sample() noexcept {
     _currentMillis = millis();
     if (_currentMillis - _previousMillis > _samplingPeriod) {
@@ -72,6 +121,11 @@ bool Oscillator::next_sample() noexcept {
     return false;
 }
 
+/**
+ * @brief Attaches the oscillator to a servo motor.
+ * @param pin The pin number to attach the servo to.
+ * @param rev If true, the sine wave will be inverted.
+ */
 void Oscillator::attach(int pin, bool rev) noexcept {
     if (!_servo.attached()) {
         _servo.attach(pin);
@@ -96,11 +150,18 @@ void Oscillator::attach(int pin, bool rev) noexcept {
     }
 }
 
+/**
+ * @brief Detaches the oscillator from the servo motor.
+ */
 void Oscillator::detach() noexcept {
     if (_servo.attached())
         _servo.detach();
 }
 
+/**
+ * @brief Sets the period of the sine wave in microseconds.
+ * @param T The period of the sine wave.
+ */
 void Oscillator::SetT(unsigned int T) noexcept {
     _period = T;
 
@@ -108,10 +169,17 @@ void Oscillator::SetT(unsigned int T) noexcept {
     _inc = 2 * M_PI / _numberSamples;
 }
 
+/**
+ * @brief Sets the position of the servo motor.
+ * @param position The position of the servo motor.
+ */
 void Oscillator::SetPosition(int position) noexcept {
     write(position);
 }
 
+/**
+ * @brief Updates the oscillator's internal state.
+ */
 void Oscillator::refresh() noexcept {
     if (next_sample()) {
         if (!_stop) {
@@ -125,6 +193,10 @@ void Oscillator::refresh() noexcept {
     }
 }
 
+/**
+ * @brief Writes the position of the servo motor.
+ * @param position The position of the servo motor.
+ */
 void Oscillator::write(int position) noexcept {
     long currentMillis = millis();
     if (_diff_limit > 0) {

a1f0337e212f9dfcf6a30dcb68ec492e02eda724
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 01:07:56 +0600

    comment

--- src/Oscillator.h
+++ src/Oscillator.h
@@ -1,4 +1,25 @@
-//--------------------------------------------------------------
-//-- Oscillator.h
-//-- Generate sinusoidal oscillations in the servos
-//--------------------------------
+class Oscillator {
+  private:
+    // Sinusoidal oscillation parameters
+    float frequency; // The frequency of the oscillation in Hz
+    float amplitude; // The amplitude of the oscillation in degrees
+    float phase; // The phase shift of the oscillation in radians
+
+  public:
+    // Constructor
+    Oscillator(float freq, float amp, float pha);
+
+    // Accessors
+    float getFrequency() const;
+    float getAmplitude() const;
+    float getPhase() const;
+
+    // Mutators
+    void setFrequency(float freq);
+    void setAmplitude(float amp);
+    void setPhase(float pha);
+
+    // Method to calculate the next oscillator value
+    float nextValue();
+};
+

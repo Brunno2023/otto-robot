22590d892f2b9770dda86256458eb861da73b64e
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 01:04:26 +0600

    improve

--- examples/Otto_avoid/Otto_avoid.ino
+++ examples/Otto_avoid/Otto_avoid.ino
@@ -1,47 +1,60 @@
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Otto avoid obstacles with ultrasonic sensor sample sketch code
 //-- Otto DIY invests time and resources providing open source code and hardware, 
-//-- please support by purchasing kits from https://www.ottodiy.com/
-//-- Make sure to have installed all libraries: https://github.com/OttoDIY/OttoDIYLib
+//-- please support by purchasing kits from <https://www.ottodiy.com/>
+//-- Make sure to have installed all libraries: <https://github.com/OttoDIY/OttoDIYLib>
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #include <Otto.h>
 Otto Otto;  //This is Otto!
-//----------------------------------------------------------------------
-#define LeftLeg 2 
-#define RightLeg 3
-#define LeftFoot 4 
-#define RightFoot 5 
-#define Buzzer  13 
-#define Trigger 8 // ultrasonic sensor trigger pin
-#define Echo 9 // ultrasonic sensor echo pin
 
-long ultrasound() {
-   long duration, distance;
-   digitalWrite(Trigger,LOW);
-   delayMicroseconds(2);
-   digitalWrite(Trigger, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(Trigger, LOW);
-   duration = pulseIn(Echo, HIGH);
-   distance = duration/58;
-   return distance;
-}
+// Define the pins used by the ultrasonic sensor
+#define TRIGGER_PIN 8
+#define ECHO_PIN 9
 
+// Define the maximum distance (in cm) that the sensor can detect
+#define MAX_DISTANCE 200
+
+// Define the minimum distance (in cm) that is considered an obstacle
+#define MIN_DISTANCE 15
 
 void setup() {
-  Otto.init(LeftLeg, RightLeg, LeftFoot, RightFoot, true, Buzzer); //Set the servo pins and Buzzer pin
-  pinMode(Trigger, OUTPUT); 
-  pinMode(Echo, INPUT); 
+  Otto.init(2, 3, 4, 5, true, 13); //Set the servo pins and Buzzer pin
 
+  // Initialize the ultrasonic sensor pins
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
 
 void loop() {
-    if (ultrasound() <= 15) {
-      Otto.sing(S_surprise);
-      Otto.playGesture(OttoConfused);
-      Otto.walk(2,1000,-1); // BACKWARD x2
-      Otto.turn(3,1000,1); // LEFT x3
-    }
-    Otto.walk(1,1000,1); // FORWARD x1
+  // Measure the distance to the nearest obstacle
+  long distance = getDistance();
 
+  // If an obstacle is too close, avoid it
+  if (distance <= MIN_DISTANCE) {
+    Otto.sing(S_surprise);
+    Otto.playGesture(OttoConfused);
+    Otto.walk(2, 1000, -1); // BACKWARD x2
+    Otto.turn(3, 1000, 1); // LEFT x3
+  }
+  // Otherwise, move forward
+  else {
+    Otto.walk(1, 1000, 1); // FORWARD x1
+  }
+}
+
+// Measures the distance to the nearest obstacle using the ultrasonic sensor
+long getDistance() {
+  // Send a short pulse on the trigger pin
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+
+  // Measure the time it takes for the pulse to return
+  long duration = pulseIn(ECHO_PIN, HIGH);
+
+  // Convert the time measurement to a distance in cm
+  // The speed of sound is approximately 340 m/s, or 29 microseconds per cm
+  return duration / 29 / 2;
 }

aada9318161049ee7da12600a7f113419f1f570d
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 22:33:38 +0600

    comment

--- src/Oscillator.h
+++ src/Oscillator.h
@@ -1,25 +1,49 @@
 class Oscillator {
   private:
     // Sinusoidal oscillation parameters
-    float frequency; // The frequency of the oscillation in Hz
-    float amplitude; // The amplitude of the oscillation in degrees
-    float phase; // The phase shift of the oscillation in radians
+    // frequency: The frequency of the oscillation in Hz
+    float frequency; 
+    // amplitude: The amplitude of the oscillation in degrees
+    float amplitude; 
+    // phase: The phase shift of the oscillation in radians
+    float phase; 
 
   public:
-    // Constructor
+    // Constructor for the Oscillator class
+    // params: 
+    //   freq: The initial frequency of the oscillation in Hz
+    //   amp: The initial amplitude of the oscillation in degrees
+    //   pha: The initial phase shift of the oscillation in radians
     Oscillator(float freq, float amp, float pha);
 
-    // Accessors
+    // Accessor for the frequency of the oscillation
+    // returns: The frequency of the oscillation in Hz
     float getFrequency() const;
+
+    // Accessor for the amplitude of the oscillation
+    // returns: The amplitude of the oscillation in degrees
     float getAmplitude() const;
+
+    // Accessor for the phase shift of the oscillation
+    // returns: The phase shift of the oscillation in radians
     float getPhase() const;
 
-    // Mutators
+    // Mutator for the frequency of the oscillation
+    // params:
+    //   freq: The new frequency of the oscillation in Hz
     void setFrequency(float freq);
+
+    // Mutator for the amplitude of the oscillation
+    // params:
+    //   amp: The new amplitude of the oscillation in degrees
     void setAmplitude(float amp);
+
+    // Mutator for the phase shift of the oscillation
+    // params:
+    //   pha: The new phase shift of the oscillation in radians
     void setPhase(float pha);
 
-    // Method to calculate the next oscillator value
+    // Method to calculate and return the next value of the oscillation
+    // returns: The next value of the oscillation
     float nextValue();
 };
-

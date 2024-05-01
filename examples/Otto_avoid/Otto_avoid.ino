c376339f5acdcbe0e7d2024c6d67345b0789f44c
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 22:34:00 +0600

    comment

--- examples/Otto_avoid/Otto_avoid.ino
+++ examples/Otto_avoid/Otto_avoid.ino
@@ -1,10 +1,14 @@
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // This is the sample sketch code for Otto, an open-source DIY robot kit, to avoid obstacles using an ultrasonic sensor.
+//
 // The code is written for the Otto DIY library, which can be found at <https://github.com/OttoDIY/OttoDIYLib>.
 // Please support Otto DIY by purchasing kits from <https://www.ottodiy.com/> if you find this code helpful.
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 #include <Otto.h> // Include the Otto library
-Otto Otto;  //Create an instance of the Otto class
+
+// Create an instance of the Otto class
+Otto Otto;
 
 // Define the pins used by the ultrasonic sensor
 #define TRIGGER_PIN 8
@@ -17,7 +21,8 @@ Otto Otto;  //Create an instance of the Otto class
 #define MIN_DISTANCE 15
 
 void setup() {
-  Otto.init(2, 3, 4, 5, true, 13); //Initialize Otto by setting the servo pins and Buzzer pin
+  // Initialize Otto by setting the servo pins and Buzzer pin
+  Otto.init(2, 3, 4, 5, true, 13);
 
   // Initialize the ultrasonic sensor pins
   pinMode(TRIGGER_PIN, OUTPUT);
@@ -30,14 +35,14 @@ void loop() {
 
   // If an obstacle is too close, avoid it
   if (distance <= MIN_DISTANCE) {
-    Otto.sing(S_surprise); //Play a surprised sound
-    Otto.playGesture(OttoConfused); //Play a confused gesture
-    Otto.walk(2, 1000, -1); //Move backward twice
-    Otto.turn(3, 1000, 1); //Turn left three times
+    Otto.sing(S_surprise); // Play a surprised sound
+    Otto.playGesture(OttoConfused); // Play a confused gesture
+    Otto.walk(2, 1000, -1); // Move backward twice
+    Otto.turn(3, 1000, 1); // Turn left three times
   }
   // Otherwise, move forward
   else {
-    Otto.walk(1, 1000, 1); //Move forward once
+    Otto.walk(1, 1000, 1); // Move forward once
   }
 }
 
